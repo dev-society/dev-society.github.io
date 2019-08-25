4d9c3c61e29795b237ad6ed64e9a6248bc8781ba
suraj_sloth <suraj.mahto49@gmail.com>
2019-08-26 01:00:58 +0530

    new file:   events.html 	new file:   external/css/bootstrap.min.css 	new file:   external/css/w3.css 	new file:   external/js/bootstrap.min.js 	new file:   images/techfest19/class.jpeg 	new file:   images/techfest19/codestorm-poster.jpeg 	new file:   images/techfest19/codestorm.jpeg 	renamed:    images/devoftheyear-poster.jpeg -> images/techfest19/devoftheyear-poster.jpeg 	new file:   images/techfest19/doy1.jpeg 	new file:   images/techfest19/doy2.jpeg	new file:   images/techfest19/doy3.jpeg 	new file:   images/techfest19/inaugration.jpeg 	new file:   images/techfest19/posters.jpeg 	modified:   index.html 	new file:   members.html 	modified:   projects.html         modified:   resources/css/queries.css         modified:   resources/css/styles.css 	modified:   resources/js/scripts.js 	new file:   videos/gamedev.mp4 	new file:   videos/graphicsdev.mp4 	new file:   w3.css
    Created projects page using jquery functionality
    
    Events page made using w3.css

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -47,5 +47,60 @@ $(document).ready(function() {
 	})
 */
 
+		var graphicsdev = $("#graphicsdev");
+		var gamedev = $("#gamedev");
+		var webdev = $("#webdev") ;
+
+		gamedev.hide();
+		webdev.hide();
+
+		$("#graphicsDevBtn").click(function(){
+//			$("video").setAttribute("src" , "images/graphicsdev.mp4");
+				//$("#projects-video source").src = 'images/graphicsdev.mp4'; 
+//			
+/*			if($("#projects-video source").length){
+				$("#projects-video").empty();
+			}
+
+
+
+			$("#projects-video").html('<source src="images/graphicsdev.mp4" type="video/mp4"></source>' );
+			$("#content video")[0].load();
+			location.reload();
+*/
+				graphicsdev.show(800);
+
+				gamedev.hide();
+				webdev.hide();
+			});
+
+
+			$("#gameDevBtn").click(function(){
+		//	$("video").setAttribute("src" , "images/graphicsdev.mp4");
+/*
+				if($("#projects-video source").length){
+					$("#projects-video").empty();
+				}
+
+
+				$("#projects-video").empty();
+				$("#projects-video").html('<source src="images/gamedev.mp4" type="video/mp4"></source>' );
+				$("#content video")[0].load();
+*/				
+				gamedev.show(800);
+
+				graphicsdev.hide();
+				webdev.hide();
+
+
+			});
+
+			$("#webDBtn").click(function(){
+				webdev.show(800);
+				gamedev.hide();
+				graphicsdev.hide();
+
+			})
+
 }  
 );
