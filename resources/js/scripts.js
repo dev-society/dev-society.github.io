25c0b14e1eaefca2401c64ff75563191d17f3160
suraj_sloth <suraj.mahto49@gmail.com>
2020-01-01 23:33:55 +0530

    preloader test 1

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,4 +1,14 @@
 
+function hideLoader() {
+    $('#preloader').hide();
+}
+
+$(window).ready(hideLoader);
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
+
+
 $(document).ready(function() {
 	$(window).scroll(function() {
 
